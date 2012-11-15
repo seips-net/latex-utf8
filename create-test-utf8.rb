ca96b7f4e6dbbfebb6d8717b906ea22c1ca2485e
Christian Lask <clask@icef.com>
2012-11-15 14:40:16 +0100

    Added monospacefont for first column, removed ;;.

--- create-test-utf8.rb
+++ create-test-utf8.rb
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 in_file = File.open('latex-utf8.tex', 'r')
 out_file = File.open('test-utf8.tex', 'w')
-header = ['\documentclass[10pt,a4paper]{book}', '%', '\usepackage[left=20mm,right=20mm,top=25mm,bottom=25mm]{geometry}', '\usepackage[utf8]{inputenc}', '\usepackage[T1]{fontenc}', '\usepackage{tabularx}', '%', '\input{latex-utf8.tex}', '%', '\pagestyle{empty}', '%', '\begin{document}', '\begin{tabularx}{1\linewidth}{b{10mm}b{5mm}X}']
+header = ['\documentclass[10pt,a4paper]{book}', 
+          '%', 
+          '\usepackage[left=20mm,right=20mm,top=25mm,bottom=25mm]{geometry}', 
+          '\usepackage[utf8]{inputenc}', 
+          '\usepackage[T1]{fontenc}', 
+          '\usepackage{tabularx}',
+          '\usepackage{courier}',
+          '%', 
+          '\input{latex-utf8.tex}', 
+          '%', 
+          '\pagestyle{empty}', 
+          '%', 
+          '\begin{document}', 
+          '\begin{tabularx}{1\linewidth}{b{10mm}b{5mm}X}']
 
 header.each do |s|
   out_file.puts s
@@ -11,9 +24,9 @@ in_file.each do |line|
   if line.include?('\Declare')
     unicode_id = line[line.index(%r|\{|)+1...line.index(%r|\}|)]
     unicode_char = line[line.index(%r/[[:alnum:]]\}/)+3...line.index(%r|%|)-2]
-    comment = line[line.index(%r/}[[:space:]]%/)+6..-1].gsub('%',';;')
+    comment = line[line.index(%r/}[[:space:]]%/)+6..-1].gsub('%','')
     
-    out_file.print unicode_id + ' & ' + unicode_char + ' & ' + "\\verb+" + comment + "+ " + "\\tabularnewline"
+    out_file.print "\\texttt{#{unicode_id}}" + ' & ' + unicode_char + ' & ' + "\\verb+" + comment + "+ " + "\\tabularnewline"
     out_file.puts
   end
 end
